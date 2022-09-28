72cd146bf882e4bd2d5193a206d59d2506648c45
Rodrigo Alfonso <rodri042@gmail.com>
2022-09-28 04:40:02 -0300

    Adding navigation

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,8 @@
 if (window.location.hash) {
 	const selectedProject = document.querySelector(window.location.hash);
-	if (selectedProject)
-		selectedProject.parentNode.style = "background-color: rgba(255, 255, 255, 0.25)";
+	if (selectedProject) {
+		const parentNode = selectedProject.parentNode;
+		if (parentNode && parentNode.nodeName === "DIV")
+			selectedProject.parentNode.style = "background-color: rgba(255, 255, 255, 0.25)";
+	}
 }
