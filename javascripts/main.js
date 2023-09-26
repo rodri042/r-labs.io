d3c474cdb68a0c09d25d52181f31680c184208c9
Rodrigo Alfonso <rodri042@gmail.com>
2023-09-25 21:04:04 -0300

    Adding discord, reddit, and links

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,13 @@
-if (window.location.hash) {
+function highlight() {
+	document.querySelectorAll(".project").forEach((it) => it.style = "");
+	
 	const selectedProject = document.querySelector(window.location.hash);
 	if (selectedProject) {
 		const parentNode = selectedProject.parentNode;
 		if (parentNode && parentNode.nodeName === "DIV")
-			selectedProject.parentNode.style = "background-color: rgba(255, 255, 255, 0.25)";
+			parentNode.style = "background-color: rgba(255, 255, 255, 0.25)";
 	}
 }
+
+window.addEventListener("hashchange", highlight);
+highlight();
