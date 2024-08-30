00528fcfe116d7a1fb51474d09f3316a6e5f4343
Rodrigo Alfonso <rodri042@gmail.com>
2024-08-30 10:16:21 -0300

    Filtering

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,5 @@
 function highlight() {
+	if (!window.location.hash) return;
 	document.querySelectorAll(".project").forEach((it) => it.style = "");
 	
 	const selectedProject = document.querySelector(window.location.hash);
@@ -11,3 +12,68 @@ function highlight() {
 
 window.addEventListener("hashchange", highlight);
 highlight();
+
+// ---
+
+function scrollToElement(id) {
+	const element = document.getElementById(id);
+	if (element)
+		element.scrollIntoView({ behavior: "smooth" });
+}
+
+let currentFilter = null;
+
+function filterBy(tagName) {
+	if (currentFilter != null) {
+		document.querySelectorAll(".project").forEach((it) => {
+			it.style.display = "block";
+		});
+		document.querySelectorAll(`.index .tag`).forEach((it) => {
+			it.style.display = "block"
+		});
+		document.querySelectorAll(`.clearfilter`).forEach((it) => {
+			it.style.display = "none"
+		});
+		currentFilter = null;
+		return;
+	}
+
+	document.querySelectorAll(`.project:not([data-tag="${tagName}"])`).forEach((it) => {
+		it.style.display = "none"
+	});
+	document.querySelectorAll(`[data-tag="${tagName}"]`).forEach((it) => {
+		it.style.display = "block"
+	});
+	document.querySelectorAll(`.index .tag`).forEach((it) => {
+		it.style.display = "none"
+	});
+	document.querySelectorAll(`.clearfilter`).forEach((it) => {
+		it.style.display = "block"
+	});
+	
+	currentFilter = tagName;
+	scrollToElement("index");
+}
+
+function findTag(node) {
+	if (!node.parentElement) return null;
+
+	return node.parentElement.dataset.tag != null
+		? node.parentElement.dataset.tag
+		: findTag(node.parentElement);
+}
+
+document.querySelectorAll(".tag").forEach((node) => {
+	const isProjectButton = findTag(node) != null;
+
+	node.addEventListener("click", () => {
+		if (isProjectButton && currentFilter != null) return;
+
+		const tagName = node.dataset.tag || findTag(node);
+		filterBy(tagName);
+	});
+});
+
+document.querySelectorAll(".clearfilter").forEach((node) => {
+	node.addEventListener("click", () => filterBy(null));
+});
